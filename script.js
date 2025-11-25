404ca135042b298e238f8f305d99d9667809cef3
sakshiportfolio <sakshishinde3152006@gmail.com>
2025-11-25 13:18:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  
+body { font-family: Poppins, sans-serif; background:#eef2fa; }
+.container{ max-width:600px; margin:auto; padding:20px; text-align:center; background:#fff;
+border-radius:10px; box-shadow:0 0 10px #0002; margin-top:50px; }
+button{ background:#0078ff; color:#fff; padding:10px 20px; border:none;
+border-radius:5px; margin:10px; cursor:pointer; }
+button:hover{ background:#005bcc; }
+.quiz-box{text-align:left;}
+#options button{ width:100%; margin:5px 0; }
+ul li{background:#d0e6ff;padding:10px;margin:5px;border-radius:5px;}
